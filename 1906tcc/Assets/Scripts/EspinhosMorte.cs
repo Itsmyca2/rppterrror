471cee675f552e0f0514bdd0faa8f4eeafa3f781
GRAZIELY LINO DE OLIVEIRA <graziely.o@academico.ifrn.edu.br>
2023-10-26 11:46:17 -0300

    tudo

---
+++ 1906tcc/Assets/Scripts/EspinhosMorte.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EspinhosMorte : MonoBehaviour
+{
+   /* public GameObject espinhos;
+    public Transform espinhostortos;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (espinhos.transform.rotation >= 0f)
+        {
+            
+        }
+    }
+
+   */
+}
