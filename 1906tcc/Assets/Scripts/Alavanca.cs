9e23a192d3c87325606b45cda431ba1d1d2d4e30
GRAZIELY LINO DE OLIVEIRA <graziely.o@academico.ifrn.edu.br>
2023-11-10 17:03:04 -0300

    organizacao

--- 1906tcc/Assets/Scripts/Alavanca.cs
+++ 1906tcc/Assets/Scripts/Alavanca.cs
@@ -6,6 +6,18 @@ using UnityEngine;
 public class Alavanca : MonoBehaviour
 {
     public Animator animator;
+    public GameObject ponteI;
+    public GameObject ponteII;
+    public GameObject ponteIII;
+    public GameObject ponteIV;
+    public GameObject ponteV;
+    public GameObject ponteVI;
+
+    public bool alavancaI;
+    public bool alavancaII;
+    public bool alavancaIII;
+    
+    
     
     // Start is called before the first frame update
     void Start()
@@ -29,6 +41,22 @@ public class Alavanca : MonoBehaviour
         else
         {
             animator.SetBool("alavancaVirou", false);
+        }
+
+        if (other.gameObject.CompareTag("Player") && alavancaI)
+        {
+            ponteI.SetActive(true);
+            ponteII.SetActive(true);
+        }
+        if (other.gameObject.CompareTag("Player") && alavancaII)
+        {
+            ponteIII.SetActive(true);
+            ponteIV.SetActive(true);
+        }
+        if (other.gameObject.CompareTag("Player") && alavancaIII)
+        {
+            ponteV.SetActive(true);
+            ponteVI.SetActive(true);
         }
     }
 }
