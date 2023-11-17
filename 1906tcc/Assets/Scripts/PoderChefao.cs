ae40b239d347db35b53a2a2681a44b526bf6a031
GRAZIELY LINO DE OLIVEIRA <graziely.o@academico.ifrn.edu.br>
2023-11-17 15:37:33 -0300

    .

--- 1906tcc/Assets/Scripts/PoderChefao.cs
+++ 1906tcc/Assets/Scripts/PoderChefao.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class PoderChefao : MonoBehaviour
 {
     public float velocidadePoder;
+    public int tempoDestruir;
     public bool left;
     
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(this.gameObject, tempoDestruir);
     }
 
     // Update is called once per frame
